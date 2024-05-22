3d53e130072179431359dccd6e4507038499aa8e
Jaime Hurtado <156006941+jaimehu20@users.noreply.github.com>
2024-05-22 21:35:52 +0200

    Delete index.mjs

--- index.mjs
+++
@@ -1,6 +0,0 @@
-import { app } from './app'
-import ServerlessHttp from 'serverless-http'
-
-export const handler = ServerlessHttp(app, {
-	response: { headers: { 'Access-Control-Allow-Origin': '*' } }
-})
