7a04e9a2c45e46075d361c837f26b86b558b5b60
jaimehu20 <jaimehu20@gmail.com>
2024-05-24 10:22:28 +0200

    DEPLOY/ Server deployed

--- connection.ts
+++ connection.ts
@@ -1,5 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
 
-const HotelMirandaDB = "mongodb://localhost:27017/HotelMirandaDB"
+dotenv.config()
+
+const HotelMirandaDB = `mongodb+srv://jaimehudev:${process.env.MONGO_PASS}@miranda-api.vhdiiei.mongodb.net/?retryWrites=true&w=majority&appName=MIRANDA-API`
+console.log(process.env.MONGO_PASS)
 
 mongoose.connect(HotelMirandaDB)
+.then( () => {
+    console.log('Successfully connected to the database.')
+})
+.catch( (err) => {
+    console.error(`Error connecting to the database. n${err}`);
+})
